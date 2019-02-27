56515dba4de882d2635f2efd320c01a59b903f50
graygabrielle <gray.atkinson.adams@gmail.com>
2019-02-26 17:28:20 -0800

    finished javascript except game reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,88 @@
 let computerChoices = ["apple", "peaches", "oranges"];
 
 let answerIndex = Math.floor((Math.random() * computerChoices.length));
-console.log(answerIndex);
 
-let word = computerChoices[answerIndex];
 
-let wordArray = word.split("");
+let word = computerChoices.splice(answerIndex, 1);
 
-var userProgress = [];
+console.log(word);
+console.log(computerChoices);
+
+let wordArray = word[0].split("");
+
+let chances = 10;
+
+let wrongLetters = [];
+
+let userProgress = [];
+
+let wins = 0;
+
+let losses = 0;
+
 for(let i = 0; i < wordArray.length; i++){
     userProgress.push("_");
 }
 render();
 
 function render(){
-    document.getElementById("userprogress").textContent = userProgress.join(" ");
+    document.getElementById("user-progress").textContent = userProgress.join(" ");
 }
 
 //document.getElementById("userprogress").textContent = userProgress.join(" ");
 
-console.log(wordArray);
+// console.log(wordArray);
 
-console.log(computerChoices[answerIndex]);
+// console.log(computerChoices[answerIndex]);
 
 document.onkeyup = function(event){
     let userInput = event.key.toLowerCase();
-    console.log(userInput);
+    // console.log(userInput);
 
-    for(let j = 0; j < wordArray.length; j++){
-        if(userInput === wordArray[j]){
-            userProgress[j] = wordArray[j];
+
+    for(let j = 0; j < wordArray.length; j++) {
+        if(userInput === wordArray[j]) {
+            userProgress[j] = userInput;
+ 
         }
+ 
+ 
+        // console.log(wrongLetters);
+ 
+        // console.log(userProgress);
+ 
     }
-    console.log(userProgress);
-    render();
-}
+ 
+    if(wordArray.indexOf(userInput) === -1) {
+        wrongLetters.push(userInput);
+        document.getElementById("wrong-letters").textContent = wrongLetters.join(", ");
+        chances--;
+        document.getElementById("chances").textContent = chances;
+
+    };
+
+    if (chances === 0) {
+
+        document.getElementById("game-over").innerText = "Game Over!!!";
+        document.getElementById("user-progress").style.display = "none";
+        document.getElementById("correctword").innerText = word;
+        document.onkeyup = null;
+        losses++;
+        document.getElementById("numloss").textContent = losses;
+       
+
+   
+
+    };
+ 
+    if(userProgress.indexOf("_") === -1) {
+        wins++;
+        document.getElementById("numwins").textContent = wins;
+        document.onkeyup = null;
+
+    };
+   
+ 
+
+    render ();
+ }
